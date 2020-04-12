d1b75006fe131466a893c30765ab2e15b4fdd9af
alikurtulush92 <kopanarya92@gmail.com>
2020-04-12 11:24:24 +0100

    Flashcard quiz done

--- flashcard-quiz-app/src/App.js
+++ flashcard-quiz-app/src/App.js
@@ -1,17 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import FlashCardList from './FlashCardList'
 import './app.css'
 import axios from 'axios'
-function App() {
-  const [flashCards, setFlashCards] = useState(SAMPLE_FLASHCARDS)
 
-  useEffect(() => {
-        
+function App() {
+  const [flashCards, setFlashCards] = useState([])
+  const [categories, setCategories] = useState([])
+
+  const categoryEl = useRef()
+  const amountEl = useRef()
+
+
+  useEffect( () => {
     axios
-        .get(`https://opentdb.com/api.php?amount=10`)
+    .get(`https://opentdb.com/api_category.php?`)
+    .then(res => {
+       setCategories(res.data.trivia_categories)
+    })
+  })
+
+
+  const decodeString =(str) => {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+           
+    axios
+        .get(`https://opentdb.com/api.php`,{
+          params: {
+            amount:amountEl.current.value,
+            category:categoryEl.current.value
+          }
+        })
         .then(res => {
           setFlashCards(res.data.results.map((questionItem,index) => {
-            const answer = questionItem.correct_answer
+            const answer = decodeString(questionItem.correct_answer)
             let options = [...questionItem.incorrect_answers.map( a => decodeString(a))
               ,answer ]
            
@@ -25,48 +52,33 @@ function App() {
             }
           }))  
         })
-
-  }, [])
-
-  const decodeString =(str) => {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML = str
-    return textArea.value
-
   }
   return (
-    <div className="container">
+    <>
+      <form className='header' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='category'>Category</label>
+          <select id='category' ref={categoryEl}>
+            {categories.map(category => {
+              return <option key={category.id} value={category.id}>{category.name}
+              </option>
+            })}
+          </select>
+        </div>
+        <div className='form-group'>
+            <label htmlFor='category'>Number of Questions</label>
+            <input type='number' id='amount' min='1' step='1' defaultValue={10} ref={amountEl}/>
+        </div>
+        <div className='form-group'>
+           <button className='btn'>Generate</button>
+        </div>
+      </form>
+      <div className="container">
        <FlashCardList flashCards={flashCards}/>
-    </div>
+       </div>
+    </>
+   
   );
 }
-const SAMPLE_FLASHCARDS = [
-  {
-    id:1,
-    question:"What's 2 + 2?",
-    answer:'4',
-    options:[
-      "2",
-      "3",
-      "4",
-      "5"
-        
-    ]
 
-  },
-  {
-    id:2,
-    question:"Question 2 ?",
-    answer:'Answer',
-    options:[
-      "Answer",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4"
-        
-    ]
-
-  }
-  
-]
 export default App;
