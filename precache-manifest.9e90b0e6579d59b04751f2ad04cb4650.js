f31656aa7fd25bd04f50a686024754d09cf2e461
alikurtulush92 <kopanarya92@gmail.com>
2020-08-11 10:56:30 +0100

    Updates

---
+++ precache-manifest.9e90b0e6579d59b04751f2ad04cb4650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b642fc03e6656328bb4173f8341baa",
+    "url": "/FlashCardQuiz/index.html"
+  },
+  {
+    "revision": "6f362ef23e739c275538",
+    "url": "/FlashCardQuiz/static/css/main.3628309d.chunk.css"
+  },
+  {
+    "revision": "a7502ecd0849cdd8fb92",
+    "url": "/FlashCardQuiz/static/js/2.3b0aa1d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FlashCardQuiz/static/js/2.3b0aa1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f362ef23e739c275538",
+    "url": "/FlashCardQuiz/static/js/main.4f046dc6.chunk.js"
+  },
+  {
+    "revision": "f8e789d931397fd4ed83",
+    "url": "/FlashCardQuiz/static/js/runtime-main.2b2304c6.js"
+  }
+]);
